f88d8a1dcbac1e307c354c2757116c829c8c708a
Faustino Vasquez Limon <faustino.vasquez@mitechnologiesinc.com>
2022-09-05 14:46:29 -0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kit;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,37 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+
+        $perday = DB::table('prt.PartsKitData')
+            ->select("prt.users.name",DB::raw("count(prt.PartsKitData.KitID) as QtyCaptured"))
+            ->leftJoin('prt.users', 'prt.PartsKitData.UserID', '=', 'prt.users.id')
+            ->whereBetween('prt.PartsKitData.created_at', [Carbon::yesterday(), Carbon::now()])
+            ->groupBy("prt.users.name")
+            ->get();
+
+        $perweek = DB::table('prt.PartsKitData')
+            ->select("prt.users.name",DB::raw("count(prt.PartsKitData.KitID) as QtyCaptured"))
+            ->leftJoin('prt.users', 'prt.PartsKitData.UserID', '=', 'prt.users.id')
+            ->whereBetween('prt.PartsKitData.created_at', [Carbon::today()->subDays(7), Carbon::now()])
+            ->groupBy("prt.users.name")
+            ->get();
+
+        $permonth = DB::table('prt.PartsKitData')
+            ->select("prt.users.name",DB::raw("count(prt.PartsKitData.KitID) as QtyCaptured"))
+            ->leftJoin('prt.users', 'prt.PartsKitData.UserID', '=', 'prt.users.id')
+            ->whereBetween('prt.PartsKitData.created_at', [Carbon::today()->subDays(30), Carbon::now()])
+            ->groupBy("prt.users.name")
+            ->get();
+
+        $lifetime = DB::table('prt.PartsKitData')
+            ->select("prt.users.name",DB::raw("count(prt.PartsKitData.KitID) as QtyCaptured"))
+            ->leftJoin('prt.users', 'prt.PartsKitData.UserID', '=', 'prt.users.id')
+            ->groupBy("prt.users.name")
+            ->get();
+
+
+
+        return view('home',compact('perday','perweek','permonth','lifetime'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,159 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Home</h1>
+    <h1></h1>
 @stop
 
 @section('content')
-    <p>Welcome to the Kits-Parts website.</p>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card card-outline card-primary">
+                <div class="card-header border-0">
+                    <h3 class="card-title">Qty Processed per Day per User (Today)</h3>
+                    <div class="card-tools">
+                        <a href="#" class="btn btn-tool btn-sm">
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-striped table-valign-middle">
+                        <thead>
+                        <tr>
+                            <th>User Name</th>
+                            <th>Qty Captured</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($perday as $day)
+                            <tr>
+                                <td>
+                                    {{$day->name}}
+                                </td>
+                                <td>
+                                    {{$day->QtyCaptured}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-lg-6">
+            <div class="card card-outline card-success">
+                <div class="card-header border-0">
+                    <h3 class="card-title">Quantity Processed per Day per User (Last 7 Days)</h3>
+                    <div class="card-tools">
+                        <a href="#" class="btn btn-tool btn-sm">
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-striped table-valign-middle">
+                        <thead>
+                        <tr>
+                            <th>User Name</th>
+                            <th>Qty Captured</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($perweek as $day)
+                            <tr>
+                                <td>
+                                    {{$day->name}}
+                                </td>
+                                <td>
+                                    {{$day->QtyCaptured}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col-lg-6">
+            <div class="card card-outline card-info">
+                <div class="card-header border-0">
+                    <h3 class="card-title">Quantity Processed per Day per User (Last 30 Days)</h3>
+                    <div class="card-tools">
+                        <a href="#" class="btn btn-tool btn-sm">
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-striped table-valign-middle">
+                        <thead>
+                        <tr>
+                            <th>User Name</th>
+                            <th>Qty Captured</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($permonth as $day)
+                            <tr>
+                                <td>
+                                    {{$day->name}}
+                                </td>
+                                <td>
+                                    {{$day->QtyCaptured}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="card card-outline card-warning">
+                <div class="card-header border-0">
+                    <h3 class="card-title">Quantity Processed per Day per User (Lifetime)</h3>
+                    <div class="card-tools">
+                        <a href="#" class="btn btn-tool btn-sm">
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body table-responsive p-0">
+                    <table class="table table-striped table-valign-middle">
+                        <thead>
+                        <tr>
+                            <th>User Name</th>
+                            <th>Qty Captured</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($lifetime as $day)
+                            <tr>
+                                <td>
+                                    {{$day->name}}
+                                </td>
+                                <td>
+                                    {{$day->QtyCaptured}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 
 @stop
 
