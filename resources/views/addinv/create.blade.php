ca63a3fac6246c0709082f5d2793b96b3a034907
Faustino Vasquez Limon <faustino.vasquez@mitechnologiesinc.com>
2022-09-07 13:44:30 -0700

    steps1

--- resources/views/addinv/create.blade.php
+++ resources/views/addinv/create.blade.php
@@ -38,17 +38,22 @@
                 </div>
 
                 <div class="card-body" id="mainForm">
-                    <p>
-                        Pasos (Steps):
-                        - Escanea el código QR de la caja para agregar kits
-                        /Scan the box QR code to add kits
 
-                        - Escanea el código QR del KIT para añadirlo a la caja
-                        /Scan the KIT QR code to add it to the box
-
-                        - Una vez hecho esto, escanee la caja para cerrar la caja.
-                        /Once done, scan the box to close the box.
-                    </p>
+                       <h2>Pasos (Steps):</h2>
+                    <ul>
+                        <li>
+                            Escanea el código QR de la caja para agregar kits.<br>
+                            /Scan the box QR code to add kits<
+                        </li>
+                        <li>
+                            Escanea el código QR del KIT para añadirlo a la caja.<br>
+                            /Scan the KIT QR code to add it to the box
+                        </li>
+                        <li>
+                            Una vez hecho esto, escanee la caja para cerrar la caja.<br>
+                            /Once done, scan the box to close the box.
+                        </li>
+                    </ul>
 
                     <div class="row mb-3">
                         <div class="col-md-6">
