d15be26a8e22486464df92175f1cbcfedd40ae03
Faustino Vasquez Limon <faustino.vasquez@mitechnologiesinc.com>
2022-10-27 10:03:29 -0700

    skudays repots 12 cols

--- resources/views/reports/skus.blade.php
+++ resources/views/reports/skus.blade.php
@@ -25,8 +25,7 @@
 
     <div class="row justify-content-center">
 
-        <div class="col-md-8">
-
+        <div class="col-lg-12">
             <div class="card">
                 <div class="card-header ">
                     <h3 class="card-title mt-1 days">
